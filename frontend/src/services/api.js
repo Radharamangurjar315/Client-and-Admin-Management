dc3e097871a7bd792cf18799e16a62c56bdb47d6
Radharaman Gurjar <gurjar.radharaman03@gmail.com>
2025-12-10 04:14:17 +0530

    Frontend Production v3

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL || 'https://client-and-admin-management.onrender.com'}/api`,
+  baseURL: `${import.meta.env.VITE_API_URL}`,
   headers: {
     'Content-Type': 'application/json'
   },
